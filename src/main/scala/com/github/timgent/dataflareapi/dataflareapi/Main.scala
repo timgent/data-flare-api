f6626e077778673f12960e333f8515128ebcad63
timgent <tim.gent@gmail.com>
2021-04-07 22:27:31 +0100

    Switch to using zio instead of cats effect

--- src/main/scala/com/github/timgent/dataflareapi/dataflareapi/Main.scala
+++ src/main/scala/com/github/timgent/dataflareapi/dataflareapi/Main.scala
@@ -1,9 +1,19 @@
 package com.github.timgent.dataflareapi.dataflareapi
 
-import cats.effect.{ExitCode, IO, IOApp}
+import zio.console.putStrLn
+import zio.interop.catz.implicits._
+import zio.interop.catz.taskEffectInstance
+import zio.{ExitCode, Task, URIO, ZEnv, ZIO}
 
-object Main extends IOApp {
-  def run(args: List[String]): IO[ExitCode] = {
-    DataflareapiServer.stream[IO].compile.drain.as(ExitCode.Success)
+object Main extends zio.App {
+  def run(args: List[String]): URIO[zio.ZEnv, zio.ExitCode] = {
+    ZIO
+      .runtime[ZEnv]
+      .flatMap { implicit runtime =>
+        DataflareapiServer.stream[Task].compile.drain.as(ExitCode.success)
+      }.foldCauseM(
+      err => putStrLn(err.prettyPrint).as(ExitCode.failure),
+      _ => ZIO.succeed(ExitCode.success)
+    )
   }
 }
