7bce819ccafb5688c24073943f1e83a205157935
timgent <tim.gent@gmail.com>
2021-05-04 19:53:20 +0100

    Allow cross-origin requests

--- src/main/scala/com/github/timgent/dataflare/api/DataflareapiServer.scala
+++ src/main/scala/com/github/timgent/dataflare/api/DataflareapiServer.scala
@@ -6,11 +6,12 @@ import com.github.timgent.dataflare.api.qcresults.QcResultsRoutes
 import fs2.Stream
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.middleware.Logger
+import org.http4s.server.middleware.{CORS, CORSConfig, Logger}
 import zio.RIO
 import zio.logging.Logging
 
 import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration.DurationInt
 
 object DataflareapiServer {
   type AppEnvironment = QcResultsRepo with Logging
@@ -19,7 +20,14 @@ object DataflareapiServer {
       T: Timer[AppTask],
       concurrentEffect: ConcurrentEffect[AppTask]
   ): Stream[AppTask, Nothing] = {
-    val httpApp = QcResultsRoutes.qcResultsRoutes.orNotFound
+    val cors = CORSConfig(
+      anyOrigin = true, // TODO: Allow allowed origins to be specified in config
+      anyMethod = true,
+      allowCredentials = true,
+      maxAge = 1.day.toSeconds
+    )
+
+    val httpApp = CORS(QcResultsRoutes.qcResultsRoutes.orNotFound, cors)
 
     // With Middlewares in place for logging
     val finalHttpApp = Logger.httpApp(true, true)(httpApp)
